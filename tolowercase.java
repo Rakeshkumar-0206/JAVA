242d47e2d4ba2dcae2903f688899225aa779c569
rakesh kumar <100760255+Rakeshkumar-0206@users.noreply.github.com>
2022-03-19 10:01:27 +0530

    Create tolowercase.java

---
+++ tolowercase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+public static void main (String[] args)
+{
+    Scanner s=new Scanner(System.in);
+    String m=s.nextLine();
+    System.out.println(m.toLowerCase());
+}
+}
